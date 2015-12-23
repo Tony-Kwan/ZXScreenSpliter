e153807b6e338107335b5ba5dd4e4cf1259f17df
pygzx <pygzx@guanzhongxian.local>
2015-12-23 21:53:39 +0800

    start without dock icon

--- ZXScreenSpliter/ViewController.m
+++ ZXScreenSpliter/ViewController.m
@@ -8,104 +8,15 @@
 
 #import "ViewController.h"
 
-#import "ZXScreenSpliter.h"
-#import "ZXGlobalHotKeyMonitor.h"
 
-#import "ZXFullScreenSplitStrategy.h"   //1X1
-#import "ZXHorizontalSplitStrategy.h"   //1X2
-#import "ZXVerticalSplitStrategy.h"     //2X1
-#import "ZXQuarterSplitStrategy.h"      //2X2
-#import "ZXSuDoKuSplitStrategy.h"       //3X3
-
-@interface ViewController ()
-@property (nonatomic, strong) ZXScreenSpliter *screenSpliter;
-@property (nonatomic, strong) ZXGlobalHotKeyMonitor *hotKeyMonitor;
-@end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.screenSpliter = [[ZXScreenSpliter alloc] init];
-    
-    self.hotKeyMonitor = [[ZXGlobalHotKeyMonitor alloc] init];
-    
-    __weak typeof (&*self) weakSelf = self;
-    [self.hotKeyMonitor setOnHotKeyPressed:^(NSString *hotKey, unsigned int hotKeyIndex) {
-        NSLog(@"%@ %zd", hotKey, hotKeyIndex);
-        
-        id<ZXScreenSplitStrategy> splitStrategy = [weakSelf createNormalStrategyWithIndex:hotKeyIndex];
-        if(splitStrategy != nil) {
-            [weakSelf splitScreenWithSplitStrategy:splitStrategy];
-        }
-    }];
+
 }
 
-- (void) splitScreenWithSplitStrategy:(id<ZXScreenSplitStrategy>)strategy {
-    CGRect dstFrame = [strategy calculateDstFrame];
-    NSLog(@"dstFrame = %@  visiableFrame = %@  ScreenFrame = %@  moveSucc = %d   meunBarHeight = %f", NSStringFromRect(dstFrame), NSStringFromRect([NSScreen mainScreen].visibleFrame), NSStringFromRect([NSScreen mainScreen].frame), [self.screenSpliter moveTopWindowToFrame:dstFrame], [[[NSApplication sharedApplication] mainMenu] menuBarHeight]);
-    NSLog(@"----------------------------------------");
-}
-
-- (id<ZXScreenSplitStrategy>) createNormalStrategyWithIndex:(unsigned int)index {
-    id<ZXScreenSplitStrategy> strategy = nil;
-    
-    switch (index) {
-        case 0 : {
-            strategy = [[ZXQuarterSplitStrategy alloc] init];
-            ((ZXQuarterSplitStrategy*)strategy).toIndex = 0;
-        }
-            break;
-        case 1 : {
-            strategy = [[ZXVerticalSplitStrategy alloc] init];
-            ((ZXVerticalSplitStrategy*)strategy).toTop = YES;
-        }
-            break;
-        case 2 : {
-            strategy = [[ZXQuarterSplitStrategy alloc] init];
-            ((ZXQuarterSplitStrategy*)strategy).toIndex = 1;
-        }
-            break;
-        case 3 : {
-            strategy = [[ZXHorizontalSplitStrategy alloc] init];
-            ((ZXHorizontalSplitStrategy*)strategy).toLeft = YES;
-        }
-            break;
-        case 4 : {
-            strategy = [[ZXFullScreenSplitStrategy alloc] init];
-        }
-            break;
-        case 5 : {
-            strategy = [[ZXHorizontalSplitStrategy alloc] init];
-            ((ZXHorizontalSplitStrategy*)strategy).toLeft = NO;
-        }
-            break;
-        case 6 : {
-            strategy = [[ZXQuarterSplitStrategy alloc] init];
-            ((ZXQuarterSplitStrategy*)strategy).toIndex = 2;
-        }
-            break;
-        case 7 : {
-            strategy = [[ZXVerticalSplitStrategy alloc] init];
-            ((ZXVerticalSplitStrategy*)strategy).toTop = NO;
-        }
-            break;
-        case 8 : {
-            strategy = [[ZXQuarterSplitStrategy alloc] init];
-            ((ZXQuarterSplitStrategy*)strategy).toIndex = 3;
-        }
-            break;
-        default:
-            break;
-    }
-    return strategy;
-}
-
-- (id<ZXScreenSplitStrategy>) create3X3StrategyWithIndex:(unsigned int)index {
-    ZXSuDoKuSplitStrategy *sudokuStrategy = [[ZXSuDoKuSplitStrategy alloc] init];
-    sudokuStrategy.toIndex = index;
-    return sudokuStrategy;
-}
 
 @end
